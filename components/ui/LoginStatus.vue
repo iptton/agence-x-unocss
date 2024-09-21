542fceab490aa4621f9b52c40ddeb784cd39aa02
iptton <iptton@gmail.com>
2024-09-22 00:20:48 +0800

    ui

--- components/ui/LoginStatus.vue
+++ components/ui/LoginStatus.vue
@@ -1,13 +1,11 @@
 <template>
   <client-only>
-    <nuxt-link v-if="isLogged" to="/console" flex justify-center items-center w-full sm-w-max px6 h12 bg-emerald-600
-      rd-full relative overflow-hidden border="~ transparent hover:[emerald7]" duration-300 ease-linear
-      un-after="absolute content-empty inset-x-0 aspect-square scale-0 op-70 origin-center duration-300 ease-linear rd-full top-0 left-0 bg-[emerald7]">
-      <span relative z-10 text-white>
-        Welcome! {{ $session.user.email }}
-      </span>
-      <span class="btn b-t-secondary" @click="logout">Log Out</span>
-    </nuxt-link>
+    <span v-if="isLogged" to="/console" px6 h12 rd-full relative text-primary>
+      <nuxt-link to="/console" mr-2>
+        Welcome! {{ username }}
+      </nuxt-link>
+      <span class="btn btn-secondary" @click="logout">Log Out</span>
+    </span>
     <nuxt-link v-else to="/login">
       <span class="btn btn-primary rounded">
         <span class="text-primary-content">Sign In</span>
@@ -18,7 +16,7 @@
 
 <script setup lang="ts">
 import { useLogged } from '~/utils/session-auth';
-let { isLogged } = useLogged();
+let { isLogged, username } = useLogged();
 
 function logout() {
   fetch('/api/logout').then(() => {
